bdad7bc6e12fc2d4215937c8fc5a848eb7cf3594
wu_jia <wu_jia@worksap.co.jp>
2018-09-20 11:51:12 +0800

    ama sep oa finish

--- src/practise/KNearestPoints.java
+++ src/practise/KNearestPoints.java
@@ -1,6 +1,82 @@
 package practise;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+import java.util.PriorityQueue;
+
 // important
 public class KNearestPoints {
-
+	public static void main(String []args) {
+		List<List<Integer>> locations = new ArrayList<>();
+		locations.add(Arrays.asList(5, 5));
+		locations.add(Arrays.asList(2, 6));
+		locations.add(Arrays.asList(7, 3));
+		locations.add(Arrays.asList(1, 1));
+		locations.add(Arrays.asList(10, 1));
+		locations.add(Arrays.asList(5, 7));
+		locations.add(Arrays.asList(9, 9));
+		locations.add(Arrays.asList(4, 9));
+		locations.add(Arrays.asList(8, 8));
+		locations.add(Arrays.asList(2, 6));
+		List<Integer> user = Arrays.asList(0, 0);
+		int k = 3;
+		
+		printResult(findKNearestLocations(locations, user, k));
+	}
+	
+	public static List<List<Integer>> findKNearestLocations(List<List<Integer>> locations, List<Integer> user, int k){
+		List<List<Integer>> result = new ArrayList<>();
+//		class Point{
+//			double x;
+//			double y;
+//			Point(double x, double y){
+//				this.x = x;
+//				this.y = y;
+//			}
+//		}
+		PriorityQueue<List<Integer>> q = new PriorityQueue<>(k, new Comparator<List<Integer>>() {
+			@Override
+			public int compare(List<Integer> o1, List<Integer> o2) {
+				double d1 = getDistance(o1, user);
+				double d2 = getDistance(o2, user);
+				return d1 < d2 ? 1 : (d1 == d2 ? 0 : -1);
+			}
+		});
+		
+		for(List<Integer> location : locations) {
+			if(q.size() < k) {
+				q.add(location);
+//				System.out.println("location in1 : " + location.get(0) + ", " + location.get(1));
+				continue;
+			}
+			if(getDistance(location, user) < getDistance(q.peek(), user)) {
+				List<Integer> out = q.remove();
+//				System.out.println("location out : " + out.get(0) + ", " + out.get(1));
+				q.add(location);
+//				System.out.println("location in2 : " + location.get(0) + ", " + location.get(1));
+			}
+		}
+		
+		while(!q.isEmpty()) {
+			result.add(q.poll());
+		}
+		
+		return result;
+	}
+	
+	public static double getDistance(List<Integer> l1, List<Integer> l2) {
+		return Math.sqrt(Math.pow(l1.get(0) - l2.get(0), 2) + Math.pow(l1.get(1) - l2.get(1), 2));
+	}
+	
+	public static void printResult(List<List<Integer>> result) {
+		for(List<Integer> list : result) {
+			System.out.print("[");
+			for(Integer i : list) {
+				System.out.print(i + " ");
+			}
+			System.out.println("]");
+		}
+	}
 }
--- src/practise/TwoSumClosest.java
+++ src/practise/TwoSumClosest.java
@@ -1,7 +1,65 @@
 package practise;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Comparator;
+import java.util.List;
+
 // Sep.
 //2sum，不超过target且尽可能大，要求输出所有pair
 public class TwoSumClosest {
-
+	public static void main(String []args) {
+		List<List<Integer>> forwarding = new ArrayList<>(); 
+		List<List<Integer>> returns = new ArrayList<>();
+		forwarding.add(Arrays.asList(1, 1000));
+		forwarding.add(Arrays.asList(2, 7000));
+		forwarding.add(Arrays.asList(3, 12000));
+		returns.add(Arrays.asList(1, 10000));
+		returns.add(Arrays.asList(2, 9000));
+		returns.add(Arrays.asList(3, 3000));
+		returns.add(Arrays.asList(4, 2000));
+		printResult(sumClosest(forwarding, returns, 11000));
+	}
+	
+	public static List<List<Integer>> sumClosest(List<List<Integer>> forwarding, List<List<Integer>> returns, int m){
+		Comparator<List<Integer>> cmp = new Comparator<List<Integer>>() {
+			@Override
+			public int compare(List<Integer> o1, List<Integer> o2) {
+				// TODO Auto-generated method stub
+				return o1.get(1) > o2.get(1) ? 1 : (o1.get(1) == o2.get(1) ? 0 : -1);
+			}
+		};
+		forwarding.sort(cmp);
+		returns.sort(cmp);
+		
+		int closest = 0;
+		List<List<Integer>> result = new ArrayList<>();
+		int i = 0, j = returns.size() - 1; 
+		while(i < forwarding.size() && j >= 0) {
+			int sum = forwarding.get(i).get(1) + returns.get(j).get(1);
+			if(sum >= m) {
+				j --;
+			}else {
+				if(m - sum < m - closest) {// closer
+					closest = sum;
+					result.clear();
+					result.add(Arrays.asList(forwarding.get(i).get(0), returns.get(j).get(0)));
+				}else if (m - sum == m - closest) {
+					result.add(Arrays.asList(forwarding.get(i).get(0), returns.get(j).get(0)));
+				}
+				i ++;
+			}
+		}
+		return result;
+	}
+	
+	public static void printResult(List<List<Integer>> result) {
+		for(List<Integer> list : result) {
+			System.out.print("[");
+			for(Integer i : list) {
+				System.out.print(i + " ");
+			}
+			System.out.println("]");
+		}
+	}
 }
